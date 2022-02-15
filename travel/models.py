b185d7c30ed42e785cdf77c875a77d0c8b5c8916
장서진 <jangseojin@jangseojins-MacBook-Pro.local>
2022-02-16 03:47:37 +0900

    게시판 뼈대

--- travel/models.py
+++ travel/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    postname = models.CharField(max_length=50)
+    contents = models.TextField(null=False)
+    writer = models.CharField(max_length=10, null=False, default='')
+    
+    date =  models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)    
+    
+    # 게시글의 제목(postname)이 Post object 대신하기
+    def __str__(self):
+        return self.postname
--- travel/views.py
+++ travel/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .models import Post
 
 # Create your views here.
+def index(request):
+    return render(request,'travel/index.html')
+
+def review(request):
+    postlist = Post.objects.all()
+    return render(request, 'travel/review.html', {'postlist':postlist})
+
+def posting(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'travel/posting.html', {'post':post})
+
+def new_post(request):
+    if request.method == "POST":
+        writer = request.POST['writer']
+        postname = request.POST['postname']
+        contents = request.POST['contents']
+        post = Post(writer=writer, postname=postname, contents=contents)
+        post.save()
+        return redirect(reverse('review'))
+    else:
+        return render(request, 'travel/new_post.html')
